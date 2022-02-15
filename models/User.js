77c9f8de5a3eb23f22c1f155baa8c50c92dcb710
George Geils <95538913+ggeils@users.noreply.github.com>
2022-02-15 12:48:58 -0500

    removed email section from user.js

--- models/User.js
+++ models/User.js
@@ -15,14 +15,6 @@ User.init(
         type: DataTypes.STRING,
         allowNull: false,
       },
-      email: {
-        type: DataTypes.STRING,
-        allowNull: false,
-        unique: true,
-        validate: {
-          isEmail: true,
-        },
-      },
       password: {
         type: DataTypes.STRING,
         allowNull: false,
