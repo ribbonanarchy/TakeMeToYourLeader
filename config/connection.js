4ebf708dbe2376153c384d7a037ab4047de3f355
Zackery Arsement <zarsement@hotmail.com>
2022-02-22 18:31:50 -0600

    Added host name for JawsDB in connection file

--- config/connection.js
+++ config/connection.js
@@ -5,7 +5,13 @@ let sequelize;
 
 // create a connection object for the database
 if (process.env.JAWSDB_URL) {
-  sequelize = new Sequelize(process.env.JAWSDB_URL);
+  sequelize = new Sequelize(process.env.JAWSDB_URL, {
+    host: 'i0rgccmrx3at3wv3.cbetxkdyhwsb.us-east-1.rds.amazonaws.com',
+    dialect: 'mysql',
+    dialectOptions: {
+      decimalNumbers: true,
+    }
+  });
 } else {
   sequelize = new Sequelize(
     process.env.DB_NAME,
